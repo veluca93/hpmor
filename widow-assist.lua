739be551918b109a065ac9fc8796733de2a762c8
Jeremie Knuesel <knuesel@gmail.com>
2017-06-19 10:23:06 +0200

    widow-assit: different colors for -1/+1 looseness
    Also different color for +1 looseness with or
    without emergency stretch.

--- widow-assist.lua
+++ widow-assist.lua
@@ -12,16 +12,32 @@ local WHAT = node.id("whatsit")
 local COL = node.subtype("pdf_colorstack")
 
 -- Make nodes for beginning and end of colored regions
-local color_push = node.new(WHAT,COL)
+local color_p1 = node.new(WHAT,COL)
+local color_p1s = node.new(WHAT,COL)
+local color_m1 = node.new(WHAT,COL)
+local color_pm1 = node.new(WHAT,COL)
+local color_pm1s = node.new(WHAT,COL)
 local color_pop = node.new(WHAT,COL)
-color_push.stack = 0
+color_p1.stack = 0
+color_p1.command = 1
+color_p1.data = "0 0 1 rg" -- PDF code for RGB blue
+color_p1s.stack = 0
+color_p1s.command = 1
+color_p1s.data = "0 0.7 0.7 rg" -- PDF code for RGB dark cyan
+color_m1.stack = 0
+color_m1.command = 1
+color_m1.data = "1 0 0 rg" -- PDF code for RGB red
+color_pm1.stack = 0
+color_pm1.command = 1
+color_pm1.data = "1 0 1 rg" -- PDF code for RGB magenta
+color_pm1s.stack = 0
+color_pm1s.command = 1
+color_pm1s.data = "1 .5 .5 rg" -- PDF code for RGB pink
 color_pop.stack = 0
-color_push.data = "1 0 0 rg" -- PDF code for RGB red
-color_push.command = 1
 color_pop.command = 2
 
--- Set to true when the next post-linebreak filter should color the last line
-local ColorLastLine = false
+-- Color to use for last line in the next post-linebreak filter call (nil = no color)
+local LastLineColor = nil
 
 -- Function to color the last line in the given list
 local color_last_line = function (n)
@@ -32,27 +48,58 @@ local color_last_line = function (n)
     end
 
     -- Surround it with color start/stop
-    lastLine.head = nodeinsertbefore(lastLine.head, lastLine.head, nodecopy(color_push))
+    lastLine.head = nodeinsertbefore(lastLine.head, lastLine.head, nodecopy(LastLineColor))
     nodeinsertafter(lastLine.head, nodetail(lastLine.head), nodecopy(color_pop))
 end
 
--- Callback to check if decreasing the looseness would decrease the line count
+-- Callback to check if changing the looseness by +-1 would affect the line count
 local pre_linebreak_test_looseness = function (head, groupeCode)
+    -- Disable underfull and overfull boxes reporting
+    luatexbase.add_to_callback("hpack_quality", function() end, "hpqfilter")
+    luatexbase.add_to_callback("vpack_quality", function() end, "vpqfilter")
+
+    -- Build a copy of the paragraph with increased looseness and no emergency stretch
+    local nP1, iP1 = tex.linebreak(nodecopylist(head), {looseness=tex.looseness+1, emergencystretch=0})
+
+    -- Build a copy of the paragraph with increased looseness and default emergency stretch
+    local nP1s, iP1s = tex.linebreak(nodecopylist(head), {looseness=tex.looseness+1})
+
     -- Build a copy of the paragraph with decreased looseness
     local nM1, iM1 = tex.linebreak(nodecopylist(head), {looseness=tex.looseness-1})
 
     -- Build a copy of the paragraph normally
     local n, i = tex.linebreak(nodecopylist(head))
+    
+    -- Reenable underfull and overfull boxes reporting
+    luatexbase.remove_from_callback("hpack_quality", "hpqfilter")
+    luatexbase.remove_from_callback("vpack_quality", "vpqfilter")
 
-    -- Store whether decreasing the looseness does decrease the line count, for "post" callback
-    ColorLastLine = iM1.prevgraf < i.prevgraf
+    -- Set color to use in the post-linebreak callback
+    if iP1.prevgraf > i.prevgraf and iM1.prevgraf < i.prevgraf then
+        -- Both +1 and -1 looseness would work
+        LastLineColor = color_pm1
+    elseif iP1s.prevgraf > i.prevgraf and iM1.prevgraf < i.prevgraf then
+        -- Both +1 and -1 looseness would work, but +1 only with emergency stretch
+        LastLineColor = color_pm1s
+    elseif iP1.prevgraf > i.prevgraf then
+        -- Only +1 looseness would work
+        LastLineColor = color_p1
+    elseif iP1s.prevgraf > i.prevgraf then
+        -- Only +1 looseness would work and only thanks to the emergency stretch
+        LastLineColor = color_p1s
+    elseif iM1.prevgraf < i.prevgraf then
+        -- Only -1 looseness would work
+        LastLineColor = color_m1
+    else
+        LastLineColor = nil
+    end
 
     return true
 end
 
 -- Callback to colorize the last line of the paragraph when ColorLastLine is true
 local post_linebreak_color_last_line = function (head, groupcode)
-    if ColorLastLine then
+    if LastLineColor then
         color_last_line(head)
     end
     return true
